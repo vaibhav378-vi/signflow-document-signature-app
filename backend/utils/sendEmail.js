ca38c0c811352f833fe9d6b587c4012682def15a
Vaibhav Bansal <vaibhavbansal378gmail.com>
2026-06-19 12:31:25 +0530

    Add email invite and public signing link

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -1,9 +1,13 @@
 import nodemailer from "nodemailer";
 
 const sendEmail = async ({ to, subject, html }) => {
+  console.log("EMAIL_USER:", process.env.EMAIL_USER);
+  console.log("EMAIL_PASS exists:", Boolean(process.env.EMAIL_PASS));
+
   const transporter = nodemailer.createTransport({
-    service: "gmail",
-    secure: true,
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false,
     auth: {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASS,
@@ -13,12 +17,14 @@ const sendEmail = async ({ to, subject, html }) => {
     },
   });
 
-  await transporter.sendMail({
+  const info = await transporter.sendMail({
     from: `"SignFlow" <${process.env.EMAIL_USER}>`,
     to,
     subject,
     html,
   });
+
+  console.log("EMAIL SENT:", info.messageId);
 };
 
 export default sendEmail;
